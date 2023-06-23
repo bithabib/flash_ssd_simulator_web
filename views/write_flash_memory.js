81ceb4acb712ff548c771c7eeaa66fda478befd9
HabiburRahman1 <habib.usa2014@gmail.com>
2023-06-23 15:16:13 +0900

    update

--- views/write_flash_memory.js
+++ views/write_flash_memory.js
@@ -15,19 +15,21 @@ function readTableRow(tableName, rowNumber, fileSizeInKB) {
 // Selecting page table and store LA and PA
 var mapping_table_row = 1;
 function selectRowMappingTable(rowNumber, logicalAddress, physicalAddress) {
-    var table = document.getElementById("mapping_table");
-    var rows = table.getElementsByTagName("tr");
-    
-    if (rowNumber >= 0 && rowNumber < rows.length) {
-        var row = rows[rowNumber];
-        row.style.backgroundColor = "green";
-        row.cells[0].innerHTML = logicalAddress;
-        row.cells[1].innerHTML = physicalAddress;
-    } else {
-        console.log("Invalid row number.");
-    }
-    mapping_table_row++;
+  var table = document.getElementById("mapping_table");
+  var rows = table.getElementsByTagName("tr");
+
+  if (rowNumber >= 0 && rowNumber < rows.length) {
+    var row = rows[rowNumber];
+    row.style.backgroundColor = "green";
+    row.cells[0].innerHTML = logicalAddress;
+    row.cells[1].innerHTML = physicalAddress;
+  } else {
+    console.log("Invalid row number.");
+  }
+  mapping_table_row++;
 }
+// File tracer from logical address
+var file_tracer = 0;
 // Read the uploaded file from input onchange
 function handleFileInputChange(e) {
   var fileInput = document.getElementById("fileUpload");
@@ -41,53 +43,73 @@ function handleFileInputChange(e) {
     console.log("File Size: " + fileSize + " bytes");
     // Bytes to kb 2 decimal places
     var fileSizeInKB = (fileSize / 1024).toFixed(2);
+    if (fileSizeInKB >= 512) {
+      alert("File size is too large, please select a file less than 512kb");
+      return;
+    } else {
+      var logicalAddressTracer = 0;
+      while (fileSizeInKB > 0) {
+        // List of block in flash memory (32 blocks)
+        const sequence = [
+          "p0d0p0b0ct",
+          "p0d0p0b1ct",
+          "p0d0p0b2ct",
+          "p0d0p0b3ct",
+          "p0d0p1b0ct",
+          "p0d0p1b1ct",
+          "p0d0p1b2ct",
+          "p0d0p1b3ct",
+          "p0d1p0b0ct",
+          "p0d1p0b1ct",
+          "p0d1p0b2ct",
+          "p0d1p0b3ct",
+          "p0d1p1b0ct",
+          "p0d1p1b1ct",
+          "p0d1p1b2ct",
+          "p0d1p1b3ct",
+          "p1d0p0b0ct",
+          "p1d0p0b1ct",
+          "p1d0p0b2ct",
+          "p1d0p0b3ct",
+          "p1d0p1b0ct",
+          "p1d0p1b1ct",
+          "p1d0p1b2ct",
+          "p1d0p1b3ct",
+          "p1d1p0b0ct",
+          "p1d1p0b1ct",
+          "p1d1p0b2ct",
+          "p1d1p0b3ct",
+          "p1d1p1b0ct",
+          "p1d1p1b1ct",
+          "p1d1p1b2ct",
+          "p1d1p1b3ct",
+        ];
+        // taking a random number
+        var random = Math.floor(Math.random() * 32);
+        // Get the block number from the sequence
+        var block = sequence[random];
+        // taking a random number for selecting page from 1 to 4
+        var randomPage = Math.floor(Math.random() * 3) + 1;
+        console.log("Block: " + block);
+        console.log("Page: " + randomPage);
+        if (fileSizeInKB < 4) {
+          readTableRow(block, randomPage, fileSizeInKB);
+        } else {
+          readTableRow(block, randomPage, 4);
+        }
 
-    // List of block in flash memory (32 blocks)
-    const sequence = [
-      "p0d0p0b0ct",
-      "p0d0p0b1ct",
-      "p0d0p0b2ct",
-      "p0d0p0b3ct",
-      "p0d0p1b0ct",
-      "p0d0p1b1ct",
-      "p0d0p1b2ct",
-      "p0d0p1b3ct",
-      "p0d1p0b0ct",
-      "p0d1p0b1ct",
-      "p0d1p0b2ct",
-      "p0d1p0b3ct",
-      "p0d1p1b0ct",
-      "p0d1p1b1ct",
-      "p0d1p1b2ct",
-      "p0d1p1b3ct",
-      "p1d0p0b0ct",
-      "p1d0p0b1ct",
-      "p1d0p0b2ct",
-      "p1d0p0b3ct",
-      "p1d0p1b0ct",
-      "p1d0p1b1ct",
-      "p1d0p1b2ct",
-      "p1d0p1b3ct",
-      "p1d1p0b0ct",
-      "p1d1p0b1ct",
-      "p1d1p0b2ct",
-      "p1d1p0b3ct",
-      "p1d1p1b0ct",
-      "p1d1p1b1ct",
-      "p1d1p1b2ct",
-      "p1d1p1b3ct",
-    ];
+        selectRowMappingTable(
+          mapping_table_row,
+          "f" + file_tracer + logicalAddressTracer,
+          block + randomPage
+        );
+        // decrease the file size by 4kb
+        fileSizeInKB = (fileSizeInKB-4).toFixed(2);
+        logicalAddressTracer++;
+      }
+      file_tracer++;
+    }
 
-    // taking a random number
-    var random = Math.floor(Math.random() * 32);
-    // Get the block number from the sequence
-    var block = sequence[random];
-    // taking a random number for selecting page from 1 to 4
-    var randomPage = Math.floor(Math.random() * 3) + 1;
-    console.log("Block: " + block);
-    console.log("Page: " + randomPage);
-
-    readTableRow(block, randomPage, fileSizeInKB);
-    selectRowMappingTable(mapping_table_row, "f"+mapping_table_row, block+randomPage);
+    // divide the file size by 4kb (page size) to get the number of pages
   }
 }
