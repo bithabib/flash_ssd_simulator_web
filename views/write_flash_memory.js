77b019743df0c8699fc46e2143520df62277e5c8
HabiburRahman1 <habib.usa2014@gmail.com>
2023-06-24 21:02:02 +0900

    Update file functionalities completed

--- views/write_flash_memory.js
+++ views/write_flash_memory.js
@@ -48,7 +48,7 @@ class FileMapping {
     var selectElement = document.getElementById("update_file");
     // Create a new option element
     var newOption = document.createElement("option");
-    newOption.value = lastRowOfMappingFile;
+    newOption.value = fileName;
     newOption.textContent = fileName;
     // Append the new option to the select element
     selectElement.appendChild(newOption);
@@ -58,7 +58,7 @@ class FileMapping {
     var selectElement = document.getElementById("delete_file");
     // Create a new option element
     var newOption = document.createElement("option");
-    newOption.value = lastRowOfMappingFile;
+    newOption.value = fileName;
     newOption.textContent = fileName;
     // Append the new option to the select element
     selectElement.appendChild(newOption);
@@ -68,12 +68,10 @@ class FileMapping {
     var selectElement = document.getElementById("read_file");
     // Create a new option element
     var newOption = document.createElement("option");
-    newOption.value = lastRowOfMappingFile;
+    newOption.value = fileName;
     newOption.textContent = fileName;
     // Append the new option to the select element
     selectElement.appendChild(newOption);
-
-
   }
 
   removeMapping(fileName) {
@@ -96,6 +94,7 @@ class FileMapping {
     return this.mappingEntries.find((entry) => entry.fileName === fileName);
   }
 }
+const fileMapping = new FileMapping();
 
 // Scroll the table to the selected row so that user don't have to scroll manually
 function scrollToSelectedRow(table) {
@@ -145,65 +144,124 @@ function selectRowMappingTable(rowNumber, logicalAddress, physicalAddress) {
 }
 // File tracer from logical address
 var file_tracer = 0;
+async function FileUpload(file) {
+  // Get the size of the file in bytes
+  var fileSize = file.size;
+
+  // Display the file size
+  console.log("File Size: " + fileSize + " bytes");
+  // Bytes to kb 2 decimal places
+  var fileSizeInKB = (fileSize / 1024).toFixed(2);
+  if (fileSizeInKB >= 512) {
+    alert("File size is too large, please select a file less than 512kb");
+    return;
+  } else {
+    fileMapping.addMapping(file.name, mapping_table_row, 0);
+    var logicalAddressTracer = 0;
+    // divide the file size by 16kb (block size) to get the number of blocks
+    while (fileSizeInKB > 0) {
+      // taking a random number
+      var random = Math.floor(Math.random() * sequence.length);
+      // Get the block number from the sequence
+      var block = sequence[random];
+      console.log(block);
+      // divide the file size by 4kb (page size) to get the number of pages
+      blockPageTracer = 1;
+      while (fileSizeInKB > 0 && blockPageTracer <= 4) {
+        if (fileSizeInKB < 4) {
+          readTableRow(block, blockPageTracer, fileSizeInKB);
+        } else {
+          readTableRow(block, blockPageTracer, 4);
+        }
+
+        selectRowMappingTable(
+          mapping_table_row,
+          "f" + file_tracer + logicalAddressTracer,
+          block + blockPageTracer
+        );
+        // Save the filename, logical address and mapping_table_row in the java class
+        // decrease the file size by 4kb
+        fileSizeInKB = (fileSizeInKB - 4).toFixed(2);
+        logicalAddressTracer++;
+        blockPageTracer++;
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+      // Remove the block from the sequence
+      sequence.splice(random, 1);
+    }
+    file_tracer++;
+    var getFileInformation = fileMapping.getMapping(file.name);
+    fileMapping.updateMapping(
+      getFileInformation.fileName,
+      getFileInformation.firstRowOfMappingFile,
+      mapping_table_row
+    );
+    console.log(fileMapping.getMapping(file.name));
+  }
+
+  // divide the file size by 4kb (page size) to get the number of pages
+}
 // Read the uploaded file from input onchange
-async function handleFileInputChange() {
+function handleFileInputChange() {
   var fileInput = document.getElementById("fileUpload");
   var file = fileInput.files[0];
-  // make delay based on user choice in seconds
-
-  // Check if a file is selected
   if (file) {
-    // Get the size of the file in bytes
-    var fileSize = file.size;
+    FileUpload(file);
+  }
+}
 
-    // Display the file size
-    console.log("File Size: " + fileSize + " bytes");
-    // Bytes to kb 2 decimal places
-    var fileSizeInKB = (fileSize / 1024).toFixed(2);
-    if (fileSizeInKB >= 512) {
-      alert("File size is too large, please select a file less than 512kb");
-      return;
-    } else {
-      const fileMapping = new FileMapping();
-      fileMapping.addMapping(file.name, mapping_table_row, 0);
-      var logicalAddressTracer = 0;
-      // divide the file size by 16kb (block size) to get the number of blocks
-      while (fileSizeInKB > 0) {
-        // taking a random number
-        var random = Math.floor(Math.random() * sequence.length);
-        // Get the block number from the sequence
-        var block = sequence[random];
-        console.log(block);
-        // divide the file size by 4kb (page size) to get the number of pages
-        blockPageTracer = 1;
-        while (fileSizeInKB > 0 && blockPageTracer <= 4) {
-          if (fileSizeInKB < 4) {
-            readTableRow(block, blockPageTracer, fileSizeInKB);
-          } else {
-            readTableRow(block, blockPageTracer, 4);
-          }
+// ----------------------------------------- Update File -------------------------------------------//
 
-          selectRowMappingTable(
-            mapping_table_row,
-            "f" + file_tracer + logicalAddressTracer,
-            block
-          );
-          // Save the filename, logical address and mapping_table_row in the java class
-          // decrease the file size by 4kb
-          fileSizeInKB = (fileSizeInKB - 4).toFixed(2);
-          logicalAddressTracer++;
-          blockPageTracer++;
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-        }
-        // Remove the block from the sequence
-        sequence.splice(random, 1);
-      }
-      file_tracer++;
-      var getFileInformation =  fileMapping.getMapping(file.name);
-      fileMapping.updateMapping(getFileInformation.fileName, getFileInformation.firstRowOfMappingFile, mapping_table_row);
-      console.log(fileMapping.getMapping(file.name));
-    }
+async function handleSelection(fileName) {
+  // const fileMapping = new FileMapping();
+  var getFileInformation = fileMapping.getMapping(fileName);
 
-    // divide the file size by 4kb (page size) to get the number of pages
+  console.log(getFileInformation);
+  var table = document.getElementById("mapping_table");
+  var rows = table.getElementsByTagName("tr");
+  console.log(getFileInformation.firstRowOfMappingFile);
+  console.log(getFileInformation.lastRowOfMappingFile);
+  for (
+    firstRow = getFileInformation.firstRowOfMappingFile;
+    firstRow < getFileInformation.lastRowOfMappingFile;
+    firstRow++
+  ) {
+    var row = rows[firstRow];
+    logicalAddress = row.cells[0].innerHTML;
+    physicalAddressWithRow = row.cells[1].innerHTML;
+    physicalAddressBlockRow = physicalAddressWithRow.charAt(
+      physicalAddressWithRow.length - 1
+    );
+    physicalAddress = physicalAddressWithRow.slice(0, -1);
+    console.log(logicalAddress);
+    console.log(physicalAddress);
+    console.log(physicalAddressBlockRow);
+    // Update the table row
+    row.style.backgroundColor = "yellow";
+    blockTable = document.getElementById(physicalAddress);
+    blockTable.rows[physicalAddressBlockRow].style.backgroundColor = "yellow";
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    row.cells[0].innerHTML = "";
+    row.cells[1].innerHTML = "";
+    row.style.backgroundColor = "white";
+  }
+}
+
+var selectedFileName = document.getElementById("update_file");
+// Add event listener for the "change" event
+selectedFileName.addEventListener("change", function () {
+  // Get the selected value
+  var fileName = selectedFileName.value;
+  // Display the selected value
+  console.log("Selected file name: " + fileName);
+  handleSelection(fileName);
+});
+
+// Update change handler
+function handleFileUpdate() {
+  var fileInput = document.getElementById("fileUpdate");
+  var file = fileInput.files[0];
+  if (file) {
+    FileUpload(file);
   }
 }
