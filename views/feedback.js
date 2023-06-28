09e5f49d531ea07374fa917542d9143c5b8cc171
HabiburRahman1 <habib.usa2014@gmail.com>
2023-06-28 12:34:48 +0900

    Getting feedback through email setup

--- views/feedback.js
+++ views/feedback.js
@@ -1,57 +1,64 @@
-const form = document.getElementById("feedback-form");
-const commentsContainer = document.getElementById("comments-container");
+var form_id_js = "feedbackForm";
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+function js_onSuccess() {
+  var feedbackMessage = document.getElementById("feedbackMessage");
+  feedbackMessage.innerText = "Email Successfully Sent!";
+  feedbackMessage.style.display = "block";
+  document.getElementById(form_id_js).reset();
+}
 
-  const feedbackInput = document.getElementById("feedback-input");
-  const feedbackText = feedbackInput.value.trim();
+function js_onError(error) {
+  var feedbackMessage = document.getElementById("feedbackMessage");
+  feedbackMessage.innerText = "Email could not be sent.";
+  feedbackMessage.style.display = "block";
+}
 
-  if (feedbackText !== "") {
-    const comment = createComment(feedbackText);
-    commentsContainer.prepend(comment);
+var sendButton = document.getElementById("feedbackForm").querySelector("button[type='submit']");
 
-    feedbackInput.value = "";
+function js_send(event) {
+  event.preventDefault();
+
+  sendButton.innerText = "Sending...";
+  sendButton.disabled = true;
+
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (request.readyState == 4) {
+      if (request.status == 200) {
+        js_onSuccess();
+      } else {
+        js_onError(request.response);
+      }
+      sendButton.innerText = "Submit";
+      sendButton.disabled = false;
+    }
+  };
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  var data_js = {
+    "access_token": "rp8thxqxvee0fpz3lvw6qr39",
+    "subject": "User Feedback",
+    "text": "Name: " + name + "\nEmail: " + email + "\nMessage: " + message
+  };
+
+  var params = toParams(data_js);
+
+  request.open("POST", "https://postmail.invotes.com/send", true);
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  request.send(params);
+}
+
+sendButton.addEventListener("click", js_send);
+
+function toParams(data_js) {
+  var form_data = [];
+  for (var key in data_js) {
+    form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
   }
-});
 
-function createComment(text) {
-  const comment = document.createElement("div");
-  comment.classList.add("comment");
-
-  const avatar = document.createElement("img");
-//   avatar.src = "../src/logo/user.jpg";
-  avatar.alt = "User Avatar";
-
-  const commentContent = document.createElement("div");
-  commentContent.classList.add("comment-content");
-
-  const username = document.createElement("h3");
-  username.textContent = "User";
-
-  const commentText = document.createElement("p");
-  commentText.textContent = text;
-
-  commentContent.appendChild(username);
-  commentContent.appendChild(commentText);
-
-  const commentActions = document.createElement("div");
-  commentActions.classList.add("comment-actions");
-
-  const likeButton = document.createElement("div");
-  likeButton.classList.add("like-button");
-  likeButton.innerHTML = '<i class="fas fa-thumbs-up"></i>Like';
-
-  const likeCount = document.createElement("span");
-  likeCount.classList.add("like-count");
-  likeCount.textContent = "0";
-
-  commentActions.appendChild(likeButton);
-  commentActions.appendChild(likeCount);
-
-  comment.appendChild(avatar);
-  comment.appendChild(commentContent);
-  comment.appendChild(commentActions);
-
-  return comment;
+  return form_data.join("&");
 }
