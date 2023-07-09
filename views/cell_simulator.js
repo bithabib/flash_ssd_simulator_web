fe201b9f82eb0c3d95009f96e8883bf887faedbd
HabiburRahman1 <habib.usa2014@gmail.com>
2023-07-10 08:15:28 +0900

    Moving electron

--- views/cell_simulator.js
+++ views/cell_simulator.js
@@ -1,25 +1,72 @@
+class ElectronList {
+  constructor() {
+    this.electrons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    this.removedElectrons = [];
+  }
+
+  addElectron(electron) {
+    this.electrons.push(electron);
+  }
+
+  removeElectron() {
+    if (this.electrons.length > 0) {
+      const removedElectron = this.electrons.pop();
+      this.removedElectrons.push(removedElectron);
+      return removedElectron;
+    } else {
+      return null;
+    }
+  }
+
+  getRemovedElectrons() {
+    return this.removedElectrons;
+  }
+
+  selectRandomElectrons() {
+    const selectedElectrons = [];
+    for (let i = 0; i < 3; i++) {
+      const randomIndex = Math.floor(Math.random() * this.electrons.length);
+      const selectedElectron = this.electrons.splice(randomIndex, 1)[0];
+      this.removedElectrons.push(selectedElectron);
+      selectedElectrons.push(selectedElectron);
+    }
+    return selectedElectrons;
+  }
+}
+const electronList = new ElectronList();
 var floatingGate = document.getElementById("floating_gate");
-var electron = document.getElementById("electron1");
+var electrons = document.getElementsByClassName("rounded-circle");
 
 var floatingGateX = floatingGate.getBoundingClientRect().left;
 var floatingGateY = floatingGate.getBoundingClientRect().top;
 
-var electronX = electron.getBoundingClientRect().left;
-var electronY = electron.getBoundingClientRect().top;
-// get the difference between the floating gate and electron
-// var diffX = (electronX - floatingGateX) * 0.05; // Adjust the speed of motion by changing the multiplier
+// function moveElectron(electron, animation, diffY) {
+//   console.log("moveElectron");
+//   animation -= 1;
+//   electron.style.top = animation + "px";
+//   if (diffY > Math.abs(animation)) {
+//     requestAnimationFrame(moveElectron(electron, animation, diffY));
+//   }
+// }
 
-// click write button to move electron to floating gate
-var animation = 0;
-var diffY = electronY - floatingGateY;
-function moveElectron() {
+function moveElectron(electron, animation, diffY) {
+  console.log("moveElectron");
   animation -= 1;
   electron.style.top = animation + "px";
-  if (Math.abs(diffY) > Math.abs(animation)) {
-    requestAnimationFrame(moveElectron);
+  if (diffY > Math.abs(animation)) {
+    requestAnimationFrame(() => moveElectron(electron, animation, diffY));
   }
 }
-
+function moveElectronButton() {
+  var selectRandomElectrons = electronList.selectRandomElectrons();
+  for (const randomElectron of selectRandomElectrons) {
+    // electrons[randomElectron].style.display = "none";
+    console.log(electrons);
+    var electron = electrons[randomElectron];
+    var electronY = electron.getBoundingClientRect().top;
+    var diffY = Math.abs(electronY - floatingGateY) - 10;
+    moveElectron(electron, 0, diffY);
+  }
+}
 const writeButton = document.getElementById("write_button");
-writeButton.addEventListener("click", moveElectron);
-
+writeButton.addEventListener("click", moveElectronButton);
