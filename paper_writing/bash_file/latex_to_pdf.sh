2da52af2d9197effa1d5c284f36bad0445401b49
HabiburRahman1 <habib.usa2014@gmail.com>
2024-03-04 11:09:13 +0900

    Update

--- paper_writing/bash_file/latex_to_pdf.sh
+++ paper_writing/bash_file/latex_to_pdf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-rm -rf output
-sleep 1
-mkdir output
-pdflatex /home/habib/Desktop/flash_ssd_simulator_web/paper_writing/latex_file/conference_101719.tex
-
+# read the current directory
+echo "Current directory: $PWD"
+# set it in a variable
+current_dir=$PWD
+# add /paper_writing/latex_file/conference_101719.tex with the current directory variable
+current_dir_with_file=$current_dir/paper_writing/latex_file/conference_101719.tex
+pdflatex $current_dir_with_file
 rm -rf *.hex
 rm -rf *.log
 rm -rf *.aux
-
 mv conference_101719.pdf ./static/paper/conference_101719.pdf
-# mv chapter_1.pdf ./output/chapter_1.pdf
