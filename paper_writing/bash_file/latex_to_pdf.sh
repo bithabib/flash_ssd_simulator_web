3d2cd0c0c8a239ed3fa28f231cd20458f119f604
HabiburRahman1 <habib.usa2014@gmail.com>
2024-03-04 10:22:40 +0900

    Update

--- paper_writing/bash_file/latex_to_pdf.sh
+++ paper_writing/bash_file/latex_to_pdf.sh
@@ -4,9 +4,9 @@ sleep 1
 mkdir output
 pdflatex /home/habib/Desktop/flash_ssd_simulator_web/paper_writing/latex_file/conference_101719.tex
 
-# rm -rf *.hex
+rm -rf *.hex
 rm -rf *.log
-# rm -rf *.aux
+rm -rf *.aux
 
-mv conference_101719.pdf ./output/conference_101719.pdf
+mv conference_101719.pdf ./static/paper/conference_101719.pdf
 # mv chapter_1.pdf ./output/chapter_1.pdf
