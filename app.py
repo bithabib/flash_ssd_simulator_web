931af252dba1c7f8f87b405d0b17ad565a3ac4c2
HabiburRahman1 <habib.usa2014@gmail.com>
2024-03-15 19:20:43 +0900

    Redirect to domain

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, request, redirect
 app = Flask(__name__)
 
 from views import flash_ssd_simulation
 from views import cell_simulator
 from views import paper
+
+@app.before_request
+def redirect_to_domain():
+    if request.host.startswith('3.89.197.36') and request.host.endswith(':3000'):
+        return redirect('https://ssd.qbithabib.com', code=301)
