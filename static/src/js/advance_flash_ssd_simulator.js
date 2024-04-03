3d370f897301e672a5a639a497c2ef515ff02348
HabiburRahman1 <habib.usa2014@gmail.com>
2024-04-03 22:00:05 +0900

    Render readme template for documentation

--- static/src/js/advance_flash_ssd_simulator.js
+++ static/src/js/advance_flash_ssd_simulator.js
@@ -102,7 +102,7 @@ async function upload_trace_file(event) {
     reader.onload = function (e) {
       const lines = e.target.result.split("\n");
       var file_lenght = lines.length;
-      for (let i = 0; i < file_lenght; i += 1000000) {
+      for (let i = 0; i < file_lenght; i += 100000) {
         let traceList = [];
         for (let j = i; j < i + 1000000 && j < file_lenght; j++) {
           const trace = {};
